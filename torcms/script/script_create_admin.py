3e20f3e16cfdd91e340131a5edd1002d46b70b40
bukun <bukun@osgeo.cn>
2016-10-14 09:15:10 +0800

    fix bug for create_admin script.

--- torcms/script/script_create_admin.py
+++ torcms/script/script_create_admin.py
@@ -8,17 +8,16 @@ from config_email import email_cfg
 from config import smtp_cfg
 def run_create_admin():
     post_data = {
-        'user_name': ['giser'],
-        'user_email': ['giser@osgeo.cn'],
-        'user_pass': ['131322'],
-        'privilege': ['fffff'],
+        'user_name': 'giser',
+        'user_email': 'giser@osgeo.cn',
+        'user_pass': '131322',
+        'role': 'fffff',
     }
     muser = MUser()
-    entry = muser.get_by_name(post_data['user_name'][0])
+    entry = muser.get_by_name(post_data['user_name'])
     if entry:
         pass
     else:
-
         muser.insert_data(post_data)
 
 
