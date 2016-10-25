e32f10c4c5f32a4b93b82ad8960bc678febd0d96
bukun <bukun@osgeo.cn>
2016-10-26 00:26:50 +0800

    mark.

--- torcms/script/script_whoosh.py
+++ torcms/script/script_whoosh.py
@@ -16,16 +16,17 @@ from torcms.model.post_model import MPost
 from torcms.model.info_model import MInfor as MApp
 
 from torcms.model.category_model import MCategory as  MInforCatalog
-
+from torcms.model.infor2catalog_model import MInfor2Catalog
 from config import router_post
 
 mappcat = MInforCatalog()
+minfo2tag = MInfor2Catalog()
 
 from torcms.model.wiki_model import MWiki
 from torcms.model.page_model import MPage
 
-def do_for_app(writer, rand=True, doc_type=''):
-    mpost = MApp()
+def do_for_app(writer, rand=True, kind = '',  doc_type=''):
+    mpost = MApp(kind = kind)
     if rand:
         recs = mpost.query_random(50)
     else:
@@ -35,17 +36,16 @@ def do_for_app(writer, rand=True, doc_type=''):
     for rec in recs:
         # # sleep(0.1)
         text2 = rec.title + ',' + html2text.html2text(tornado.escape.xhtml_unescape(rec.cnt_html))
-        # writer.update_document(path=u"/a",content="Replacement for the first document")
         writer.update_document(
              catid = '00000',
             title=rec.title,
-            type=doc_type,
-            link='/{0}/{1}'.format(router_post['2'], rec.uid),
+            type=doc_type[rec.kind],
+            link='/{0}/{1}'.format(router_post[rec.kind], rec.uid),
             content=text2
         )
 
 
-def do_for_app2(writer, rand=True):
+def do_for_app2(writer, rand=True ):
     mpost = MApp()
     if rand:
         recs = mpost.query_random(50)
@@ -57,17 +57,27 @@ def do_for_app2(writer, rand=True):
         # # sleep(0.1)
         text2 = rec.title + ',' + html2text.html2text(tornado.escape.xhtml_unescape(rec.cnt_html))
         # writer.update_document(path=u"/a",content="Replacement for the first document")
-        if 'def_cat_uid' in rec.extinfo:
-            print(rec.extinfo['def_cat_uid'])
-            catid = rec.extinfo['def_cat_uid'][:2] + '00'
-            writer.update_document(
-                title=rec.title,
-                catid=catid,
-                type='<span style="color:red;">[{0}]</span>'.format(
-                    mappcat.get_by_uid(rec.extinfo['def_cat_uid'][:2] + '00').name),
-                link='/{0}/{1}'.format(router_post['2'], rec.uid),
-                content=text2
-            )
+
+
+
+        info = minfo2tag.get_entry_catalog(rec.uid)
+        if info:
+            print(info.uid)
+            print(info.tag.uid)
+            print(info.tag.kind)
+            pass
+        else:
+            continue
+
+        catid = info.tag.uid[:2] + '00'
+        writer.update_document(
+            title=rec.title,
+            catid=catid,
+            type='<span style="color:red;">[{0}]</span>'.format(
+                mappcat.get_by_uid(rec.extinfo['def_cat_uid'][:2] + '00').name),
+            link='/{0}/{1}'.format(router_post[rec.kind], rec.uid),
+            content=text2
+        )
 
 
 def do_for_post(writer, rand=True, doc_type=''):
@@ -131,7 +141,7 @@ def do_for_page(writer, rand=True, doc_type=''):
             link='/page/{0}.html'.format(rec.uid),
             content=text2
         )
-def gen_whoosh_database(if_rand=True, kind='1', post_type={}):
+def gen_whoosh_database(if_rand=True, kind='1', kind_arr = [] , post_type={}):
     analyzer = ChineseAnalyzer()
     schema = Schema(title=TEXT(stored=True, analyzer=analyzer),
                     catid=TEXT(stored=True),
@@ -147,25 +157,15 @@ def gen_whoosh_database(if_rand=True, kind='1', post_type={}):
 
     writer = ix.writer()
 
-    if kind == '1':
-        do_for_app(writer, rand=if_rand, doc_type=post_type['info_type'])
-    else:
-        do_for_app2(writer, rand=if_rand)
+    do_for_app2(writer, rand=if_rand)
 
-    do_for_post(writer, rand=if_rand, doc_type=post_type['doc_type'])
-    do_for_wiki(writer, rand=if_rand, doc_type=post_type['doc_type'])
-    do_for_page(writer, rand=if_rand, doc_type=post_type['doc_type'])
+    do_for_post(writer, rand=if_rand, doc_type=post_type['1'])
+    do_for_wiki(writer, rand=if_rand, doc_type=post_type['1'])
+    do_for_page(writer, rand=if_rand, doc_type=post_type['1'])
+
+    for kind in kind_arr:
+        do_for_app(writer, rand=if_rand, kind = kind, doc_type=post_type)
     print('-' * 10)
     writer.commit()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        rand = False
-    else:
-        rand = True
-    post_type = {
-        'doc_type': '<span style="color:green;" class="glyphicon glyphicon-list-alt">[{0}]</span>'.format('文档'),
-        'info_type': '<span style="color:blue;" class="glyphicon glyphicon-map-marker">[{0}]</span>'.format('地图'),
-    }
-    gen_whoosh_database(if_rand=rand, post_type=post_type)
