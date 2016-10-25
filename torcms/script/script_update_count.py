da3c4a3a860aab6239ddcc9e0229f87a393e3cec
bukun <bukun@osgeo.cn>
2016-10-25 12:31:46 +0800

    fix a bug for map.

--- torcms/script/script_update_count.py
+++ torcms/script/script_update_count.py
@@ -8,10 +8,12 @@ from torcms.model.infor2catalog_model import MInfor2Catalog
 def run_update_count():
     mapp2cat = MInfor2Catalog()
     mappcat = MCategory()
-    for rec in mappcat.query_all( kind = '20' ):
-        uid= rec.uid
-        print(rec.name)
-        # uuvv = mapp.query_extinfo_by_cat(uid)
-        uuvv = mapp2cat.query_by_catid(rec.uid)
-        print(uid, uuvv.count())
-        mappcat.update_count(uid, uuvv.count())
+
+    for kd in ['1', '2', 'm', 's']:
+        for rec in mappcat.query_all( kind = kd ):
+            uid= rec.uid
+            print(rec.name)
+            # uuvv = mapp.query_extinfo_by_cat(uid)
+            uuvv = mapp2cat.query_by_catid(rec.uid)
+            print(uid, uuvv.count())
+            mappcat.update_count(uid, uuvv.count())
